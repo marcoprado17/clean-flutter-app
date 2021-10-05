83ce06dcb104e3726fd9c7492c2a303474b0f4d4
Marco Aurélio Prado <marco.pdsv@gmail.com>
2021-10-05 12:30:29 -0300

    feat: update app colors

--- lib/ui/components/app_theme.dart
+++ lib/ui/components/app_theme.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 ThemeData makeAppTheme() {
-  final primaryColor = Color.fromRGBO(136, 14, 79, 1);
-  final primaryColorDark = Color.fromRGBO(96, 0, 39, 1);
-  final primaryColorLight = Color.fromRGBO(188, 71, 123, 1);
+  final primaryColor = Color.fromRGBO(10, 150, 79, 1);
+  final primaryColorDark = Color.fromRGBO(10, 240, 39, 1);
+  final primaryColorLight = Color.fromRGBO(10, 180, 123, 1);
   final secondaryColor = Color.fromRGBO(0, 77, 64, 1);
   final secondaryColorDark = Color.fromRGBO(0, 37, 26, 1);
   final disabledColor = Colors.grey[400];
